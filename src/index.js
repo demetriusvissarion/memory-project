1b1774e415687009b540e2e7f0859df363e22143
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-10 16:09:57 +0100

    JSX structure

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <App />
+  </div>
+);
