b489f1e2af02bdd9532bd44628f72fa8633280af
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-07-10 17:30:21 +0100

    started redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import posts from "./reducers/posts";
+
+// import reducers from './reducers';
 
 import App from "./App";
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+// const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+const store = configureStore({
+  reducer: {
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+    posts, // posts: posts,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div>
+  <Provider>
     <App />
-  </div>
+  </Provider>
 );
+
+export default store;
